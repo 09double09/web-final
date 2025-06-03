90da6e70f2574bab98f518e97d369998e39cca6f
09double09 <nm40209@gmail.com>
2025-06-03 15:48:02 +0800

    feat:back ground

---
+++ src/app/pageIfo.js
@@ -0,0 +1,62 @@
+import arrow from "@/app/imgs/games/arrow.png";
+import bacteria from "@/app/imgs/games/bacteria.png";
+import cake from "@/app/imgs/games/cake.png";
+import CD from "@/app/imgs/games/CD.png";
+import chair from "@/app/imgs/games/chair.png";
+import pen from "@/app/imgs/games/pen.png";
+import claw from "@/app/imgs/games/claw.png";
+import tomato from "@/app/imgs/games/tomato.png";
+import joker from "@/app/imgs/games/joker.png";
+
+const games = [
+  {
+    id: 1,
+    name: "標靶射擊遊戲",
+    description: "射擊遊戲",
+    image: arrow,
+  },
+//   {
+//     id: 2,
+//     name: "椅子大風吹",
+//     description: "椅子大風吹",
+//     image: chair,
+//   },
+//   {
+//     id: 3,
+//     name: "記憶對對碰",
+//     description: "記憶對對碰",
+//     image: bacteria,
+//   },
+//   {
+//     id: 4,
+//     name: "午夜唱片行",
+//     description: "午夜唱片行",
+//     image: CD,
+//   },
+//   {
+//     id: 5,
+//     name: "蔬菜守門員",
+//     description: "蔬菜守門員",
+//     image: tomato,
+//   },
+//   {
+//     id: 6,
+//     name: "夾娃娃",
+//     description: "夾娃娃",
+//     image: claw,
+//   },
+//   {
+//     id: 7,
+//     name: "紙上大作戰",
+//     description: "紙上大作戰",
+//     image: pen,
+//   },
+//   {
+//     id: 8,
+//     name: "夢色蛋糕師",
+//     description: "夢色蛋糕師",
+//     image: cake,
+//   },
+];
+
+export default games;
